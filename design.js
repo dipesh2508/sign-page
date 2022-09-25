99575b31ae02da07ea015f52b3466c4f87732df8
Dipesh Ranjan <sinhadipesh25@gmail.com>
2022-09-25 18:02:14 +0530

    first commit

---
+++ design.js
@@ -0,0 +1,9 @@
+window.onload = function () {
+    Particles.init({
+        selector: '.background',
+        maxParticles: 105,
+        connectParticles: true,
+        speed: 0.8,
+
+    });
+};
